c301b3e4d79cabef2eb7fbba5c56274a8976e362
Cleantech Solar IT <integration@cleantechsolar.com>
2025-08-22 10:34:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,26 +5,18 @@ from urllib.parse import quote_plus
 # Load environment variables from .env file
 load_dotenv()
 
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-# class Config:
-#     SECRET_KEY = os.getenv('SECRET_KEY', 'your_default_secret_key')
-
-#     SQLALCHEMY_DATABASE_URI = f"sqlite:///{os.path.join(basedir, 'instance', 'inventory.db')}"
-#     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'your_default_secret_key')
     
-    # MySQL connection settings from environment variables
-    MYSQL_USER = os.getenv('MYSQL_USER', 'root')
-    MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD', 'root')
-    MYSQL_HOST = os.getenv('MYSQL_HOST', '127.0.0.1')
-    MYSQL_PORT = os.getenv('MYSQL_PORT', '3306')
-    MYSQL_DB = os.getenv('MYSQL_DB', 'assets')
-    
-    # SQLAlchemy database URI
+    # PostgreSQL connection settings from environment variables
+    POSTGRES_USER = os.getenv('POSTGRES_USER', 'inventory_oa06_user')
+    POSTGRES_PASSWORD = os.getenv('POSTGRES_PASSWORD', 'HiafztY7PGlYOGqqUdKTDtqaAw9IQY2e')
+    POSTGRES_HOST = os.getenv('POSTGRES_HOST', 'dpg-cvhe9g52ng1s73bf72ng-a.oregon-postgres.render.com')
+    POSTGRES_PORT = os.getenv('POSTGRES_PORT', '5432')  # Default PostgreSQL port
+    POSTGRES_DB = os.getenv('POSTGRES_DB', 'inventory_oa06')
+
+    # SQLAlchemy database URI for PostgreSQL
     SQLALCHEMY_DATABASE_URI = (
-        f"mysql+pymysql://{MYSQL_USER}:{quote_plus(MYSQL_PASSWORD)}@{MYSQL_HOST}:{MYSQL_PORT}/{MYSQL_DB}"
+        f"postgresql+psycopg2://{POSTGRES_USER}:{quote_plus(POSTGRES_PASSWORD)}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
